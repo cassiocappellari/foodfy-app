10d8d62e9f3ee943b789cb8460a171ab44bab0d9
Cássio <cassiocappellari@gmail.com>
2020-06-27 11:11:25 -0300

    scripts and modal are done

---
+++ public/scripts/scripts.js
@@ -0,0 +1,19 @@
+const modalOverlay = document.querySelector(".modal-overlay")
+const cards = document.querySelectorAll(".card")
+const modalContent = document.querySelector(".modal-content")
+
+for (let card of cards) {
+    card.addEventListener("click", function() {
+        modalOverlay.classList.add("active")
+        const imageId = card.getAttribute("id")
+        const dishName = card.getElementsByTagName("h3")[0].textContent
+        const dishAuthor = card.getElementsByTagName("h4")[0].textContent
+        modalOverlay.querySelector('img').src=`../assets/${imageId}.png`
+        modalOverlay.querySelector('h3').innerHTML = `${dishName}`
+        modalOverlay.querySelector('h4').innerHTML = `${dishAuthor}`
+    })
+}
+
+document.querySelector(".close-modal").addEventListener('click', function(){
+    modalOverlay.classList.remove('active')
+})
