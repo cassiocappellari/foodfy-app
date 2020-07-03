8796d85d85d079dcfdd6d0c7496320f25bb7a12e
Cássio <cassiocappellari@gmail.com>
2020-07-03 13:15:36 -0300

    backend is done

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
+const recipes = require('./data')
 
 const server = express()
 
@@ -10,19 +11,31 @@ server.use(express.static('public/scripts'))
 server.set('view engine', 'njk')
 
 nunjucks.configure('views', {
-    express: server
+    express: server,
+    autoescape: false
 })
 
 server.get('/', function(req, res){
-    return res.render('index')
+    return res.render('index', {items: recipes})
 })
 
 server.get('/recipes', function(req, res){
-    return res.render('recipes')
+    return res.render('recipes', {items: recipes})
 })
 
 server.get('/about', function(req, res){
     return res.render('about')
+})
+
+server.get('/details/:index', function(req, res) {
+    const recipeIndex = req.params.index
+
+    const recipeData = recipes.find(function(recipe) {
+        
+    })
+
+
+    return res.render('details')
 })
 
 server.listen(5000, function(){
