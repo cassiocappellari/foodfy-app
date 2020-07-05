c1f66781760803e5c40287322ba898d7cc4a8afe
Cássio <cassiocappellari@gmail.com>
2020-07-05 15:39:37 -0300

    list details done

--- data.js
+++ data.js
@@ -13,15 +13,15 @@ module.exports = [
         '30 ml de água gelada'
       ],
       preparation: [
-        'Misture todos os ingredientes muito bem e amasse para que fique tudo muito bem misturado.',
-        'Faça porções de 90 g a 100 g.',
-        'Forre um plástico molhado em uma bancada e modele os hambúrgueres utilizando um aro como base.',
-        'Faça um de cada vez e retire o aro logo em seguida.',
+        'Misture todos os ingredientes muito bem e amasse para que fique tudo muito bem misturado',
+        'Faça porções de 90 g a 100 g',
+        'Forre um plástico molhado em uma bancada e modele os hambúrgueres utilizando um aro como base',
+        'Faça um de cada vez e retire o aro logo em seguida',
         'Forre uma assadeira de metal com plástico, coloque os hambúrgueres e intercale camadas de carne e plásticos (sem apertar).',
-        'Faça no máximo 4 camadas por forma e leve para congelar.',
-        'Retire do congelador, frite ou asse e está pronto.'
+        'Faça no máximo 4 camadas por forma e leve para congelar',
+        'Retire do congelador, frite ou asse e está pronto'
       ],
-      information: "Preaqueça a chapa, frigideira ou grelha por 10 minutos antes de levar os hambúrgueres. Adicione um pouquinho de óleo ou manteiga e não amasse os hambúrgueres! \n\n Você sabia que a receita que precede o hambúrguer surgiu no século XIII, na Europa? A ideia de moer a carne chegou em Hamburgo no século XVII, onde um açogueiro resolveu também temperá-la. Assim, a receita foi disseminada nos Estados Unidos por alemães da região. Lá surgiu a ideia de colocar o hambúrguer no meio do pão e adicionar outros ingredientes, como queijom tomates e alface."
+      information: "Preaqueça a chapa, frigideira ou grelha por 10 minutos antes de levar os hambúrgueres. Adicione um pouquinho de óleo ou manteiga e não amasse os hambúrgueres! \n\nVocê sabia que a receita que precede o hambúrguer surgiu no século XIII, na Europa? A ideia de moer a carne chegou em Hamburgo no século XVII, onde um açogueiro resolveu também temperá-la. Assim, a receita foi disseminada nos Estados Unidos por alemães da região. Lá surgiu a ideia de colocar o hambúrguer no meio do pão e adicionar outros ingredientes, como queijom tomates e alface."
     },
     {
       id: 'pizza',
