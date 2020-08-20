a8524491a58d7b640ae9350aa5f9e23ce2882e26
Cássio <cassiocappellari@gmail.com>
2020-08-20 11:54:43 -0300

    foodfy updated

--- public/scripts/site/scripts.js
+++ public/scripts/site/scripts.js
@@ -13,32 +13,38 @@ for (let card of cards) {
     })
 }
 
-button1.addEventListener('click', function(){
-    if (button1.textContent == 'HIDE') {
-        button1.innerHTML = 'SHOW'
-        displayIngredients.classList.add("hide")
-    } else {
-        button1.innerHTML = 'HIDE'
-        displayIngredients.classList.remove("hide")
-    }
-})
+if(button1) {
+    button1.addEventListener('click', function(){
+        if (button1.textContent == 'HIDE') {
+            button1.innerHTML = 'SHOW'
+            displayIngredients.classList.add("hide")
+        } else {
+            button1.innerHTML = 'HIDE'
+            displayIngredients.classList.remove("hide")
+        }
+    })
+}
 
-button2.addEventListener('click', function(){
-    if (button2.textContent == 'HIDE') {
-        button2.innerHTML = 'SHOW'
-        displayPreparation.classList.add("hide")
-    } else {
-        button2.innerHTML = 'HIDE'
-        displayPreparation.classList.remove("hide")
-    }
-})
+if(button2) {
+    button2.addEventListener('click', function(){
+        if (button2.textContent == 'HIDE') {
+            button2.innerHTML = 'SHOW'
+            displayPreparation.classList.add("hide")
+        } else {
+            button2.innerHTML = 'HIDE'
+            displayPreparation.classList.remove("hide")
+        }
+    })
+}
 
-button3.addEventListener('click', function(){
-    if (button3.textContent == 'HIDE') {
-        button3.innerHTML = 'SHOW'
-        displayInformation.classList.add("hide")
-    } else {
-        button3.innerHTML = 'HIDE'
-        displayInformation.classList.remove("hide")
-    }
-})
+if(button3) {
+    button3.addEventListener('click', function(){
+        if (button3.textContent == 'HIDE') {
+            button3.innerHTML = 'SHOW'
+            displayInformation.classList.add("hide")
+        } else {
+            button3.innerHTML = 'HIDE'
+            displayInformation.classList.remove("hide")
+        }
+    })
+}
